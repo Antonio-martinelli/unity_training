b62225912e1601abeb18a5141f55e4559742cb45
MARTINELLI Antonio <Antonio.MARTINELLI@akka.eu>
2018-08-02 16:22:47 +0200

    added player model + collectables

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -3,6 +3,12 @@
 public class PlayerCollision : MonoBehaviour {
 
     public PlayerMovement movement;
+    public int collected;
+
+    void Start()
+    {
+        collected = 0;
+    }
 
     void OnCollisionEnter(Collision collisionInfo)
     {
@@ -10,7 +16,15 @@ public class PlayerCollision : MonoBehaviour {
         {
             movement.enabled = false;
             FindObjectOfType<GameManager>().EndGame();
+        }
+    }
 
+    void OnTriggerEnter(Collider colliderInfo)
+    {
+        if (colliderInfo.tag == "Collectable")
+        {
+            colliderInfo.gameObject.SetActive(false);
+            collected = collected + 1;
         }
     }
 }
--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -4,10 +4,21 @@ using UnityEngine.UI;
 public class Score : MonoBehaviour {
 
     public Transform player;
+    public GameObject playerScript;
     public Text scoreText;
+    public Text collectibleText;
+    private string maxCollectable;
+
+    void Start()
+    {
+        maxCollectable = GameObject.FindGameObjectsWithTag("Collectable").Length.ToString();
+        collectibleText.text = playerScript.GetComponent<PlayerCollision>().collected.ToString() + "/" + GameObject.FindGameObjectsWithTag("Collectable").Length.ToString();
+    }
 
 	// Update is called once per frame
 	void Update () {
         scoreText.text = player.position.z.ToString("0");
-	}
+        collectibleText.text = playerScript.GetComponent<PlayerCollision>().collected.ToString() + "/" + maxCollectable;
+    }
+
 }
