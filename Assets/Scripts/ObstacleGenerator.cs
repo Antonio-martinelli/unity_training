de50694577c21804e4bf13312540eba68358db8b
MARTINELLI Antonio <Antonio.MARTINELLI@akka.eu>
2018-08-07 16:24:50 +0200

    added procedural scene

---
+++ Assets/Scripts/ObstacleGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleGenerator : MonoBehaviour {
+
+    public Transform player;
+    public GameObject playerObject;
+    private float initialForwardForce;
+
+    private void Start()
+    {
+        initialForwardForce = playerObject.GetComponent<PlayerMovement>().forwardForce;
+    }
+    
+
+    // Update is called once per frame
+    void FixedUpdate () {
+        
+	}
+}
