3d2ee57b021a5eb55eda2187be292b2e7180e85b
MARTINELLI Antonio <Antonio.MARTINELLI@akka.eu>
2018-08-09 15:00:17 +0200

    procedural generation work in progress

--- Assets/Scripts/ObstacleGenerator.cs
+++ Assets/Scripts/ObstacleGenerator.cs
@@ -7,15 +7,54 @@ public class ObstacleGenerator : MonoBehaviour {
     public Transform player;
     public GameObject playerObject;
     private float initialForwardForce;
+    public GameObject[] obstacles;
+    public int offset = 30;
+    public float spawnTime = 1f;
+    public float timeToWait = 0.5f;
+    public int obstaclesSpawned;
+    public int initialObstacles = 5;
+    private Transform playerInitialPosition;
 
     private void Start()
     {
+        playerInitialPosition = player.transform;
         initialForwardForce = playerObject.GetComponent<PlayerMovement>().forwardForce;
+        obstacles = Resources.LoadAll<GameObject>("Obstacles");
+        for (obstaclesSpawned = 0; obstaclesSpawned < initialObstacles; obstaclesSpawned++)
+        {
+            SpawnObstacles(obstaclesSpawned);
+        }
     }
-    
 
-    // Update is called once per frame
     void FixedUpdate () {
+        if (Time.time >= spawnTime & obstaclesSpawned >= initialObstacles)
+        {
+            SpawnObstacles(obstaclesSpawned);
+            obstaclesSpawned++;
+            spawnTime = Time.time + timeToWait;
         
-	}
+        }
+        RemovePastObject();
+
+    }
+
+    void SpawnObstacles(int obstaclesSpawned)
+    {
+        int randomIndex = Random.Range(0, obstacles.Length);
+        Instantiate(obstacles[randomIndex],
+             new Vector3(0, 1, playerInitialPosition.position.z + offset * (obstaclesSpawned + 1)),
+             Quaternion.identity);
+    }
+
+    void RemovePastObject()
+    {
+        GameObject[] obstacles = GameObject.FindGameObjectsWithTag("Obstacle");
+        foreach (GameObject obstacle in obstacles)
+        {
+            if (obstacle.transform.position.z < player.position.z - 10)
+            {
+                Destroy(obstacle);
+            }
+        }
+    }
 }
